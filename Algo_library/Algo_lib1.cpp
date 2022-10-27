1de2d952891ff7eb6ec217e00c741fe27c11f06d
read-my-name <ericmicheal50@gmail.com>
2022-10-27 22:26:10 +0800

    Add leetcode 3 and 567 algo
    From Algo_Crack 14 days plan

--- Algo_library/Algo_lib1.cpp
+++ Algo_library/Algo_lib1.cpp
@@ -15,3 +15,54 @@ int Algo_lib1::nAdd(int nA, int nB)
 	std::cout << nA + nB << "\n";
 	return nA + nB;
 }
+
+int Algo_lib1::lengthOfLongestSubstring(std::string szMsg) //All is lower case
+{
+    int nErase = 0;
+    int nInsert = 0;
+    int nLongest = 0;
+    std::unordered_set<char> us;
+
+    while (nInsert < szMsg.length())
+    {
+        if (us.find(szMsg[nInsert]) == us.end())
+        { //will only enter when there's no same char inside, Eg: null
+            us.insert(szMsg[nInsert]);
+            nLongest = nLongest > nInsert - nErase + 1 ? nLongest : nInsert - nErase + 1; //max(nLongest, nInsert - nErase + 1)
+            ++nInsert;
+        }
+        else
+        { //enter here if found same char
+            us.erase(szMsg[nErase]);
+            ++nErase;
+        }
+    }
+    return nLongest;
+}
+
+bool Algo_lib1::checkInclusion(std::string sz1, std::string sz2)
+{
+    int nMap[26] = { 0 };
+    int j = 0, i = 0, nCount_chars = sz1.size();
+
+    for (char c : sz1)
+    { // convert char into ascii value (a = 97)
+        nMap[c - 'a']++;
+    }
+    while (j < sz2.size())
+    {
+        if (nMap[sz2.at(j++) - 'a']-- > 0)
+        {
+            nCount_chars--;
+        }
+        if (nCount_chars == 0)
+        {
+            return true;
+        }
+        if (j - i == sz1.size() && nMap[sz2.at(i++) - 'a']++ >= 0)
+        {
+            nCount_chars++;
+        }
+    }
+    return false;
+}
--- Algo_library/Algo_lib1.h
+++ Algo_library/Algo_lib1.h
@@ -9,5 +9,7 @@ class Algo_lib1
 
 	//private:
 		int nAdd(int nA, int nB);
+		int lengthOfLongestSubstring(std::string szMsg); //All lower case
+		bool checkInclusion(std::string sz1, std::string sz2);
 };
 
