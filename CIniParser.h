e0b63503286e8718e8860c172ad746b39638a46f
read-my-name <116362889+read-my-name@users.noreply.github.com>
2023-08-06 19:32:58 +0800

    Update

---
+++ CIniParser.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdint>
+#include <cerrno>
+
+class CIniParser
+{
+public:
+	CIniParser();
+	~CIniParser();
+
+	std::uintmax_t getFileSize(std::string strFileName);
+	std::string strGetValue(const std::string strFilePath, const std::string& section, const std::string& key, const std::string strDefault = "", bool isAppend = false);
+	std::string strGet(const std::string strFilePath, const std::string& section, const std::string& key, const std::string strDefault = "");
+	std::string strGetAndSet(const std::string strFilePath, const std::string& section, const std::string& key, const std::string strDefault);
+
+private:
+
+};
+
